70ab49b143c752fbace2500a35e3c548e1832514
Guillermo Redondo <gredondoe@gmail.com>
2014-09-04 00:29:19 -0500

    Sigin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
   it { should respond_to(:authenticate) }
@@ -82,7 +84,7 @@ describe "with a password that's too short" do
     it { should be_invalid }
   end
 
-  describe "return value of authenticate method" do
+describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by(email: @user.email) }
 
@@ -96,6 +98,11 @@ describe "with a password that's too short" do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
